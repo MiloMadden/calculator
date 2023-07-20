6a8d426f010df00e89abd11f7d813c61e5ef018f
MiloMadden <milogotze@outlook.com>
2023-07-20 12:03:34 -0500

    a

--- front/src/components/Dashboard.jsx
+++ front/src/components/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
   
       const fetchCoins = async()=>{
   
-        const { data }  = await axios.get("https://calculator-pied-three-52.vercel.app/api/get_coins")
+        const { data }  = await axios.get("/api/get_coins")
   
         console.log(data.coins)
         setCoins(data.coins)
