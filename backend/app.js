859197ccfcb69b460984410ff32fcffc8c45631a
MiloMadden <milogotze@outlook.com>
2023-07-20 12:12:41 -0500

    aaaa

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,12 @@ const corsOptions = {
             return callback(null, origin)
         }
         return callback("Error de Cors perra")
-    }
+    }, 
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization'
 }
 
-app.use( cors() )
+app.use( cors(corsOptions) )
 app.use( express.json() )
 app.use( express.static('public') )
 
