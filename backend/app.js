5d0b9ee8b048286fb055162eb4f63f6c42c4907a
MiloMadden <milogotze@outlook.com>
2023-07-20 00:13:57 -0500

    six1

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,6 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173', 'https://calculator-pied-three-52.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-
 app.use( cors() )
 app.use( express.json() )
 app.use( express.static('public') )
